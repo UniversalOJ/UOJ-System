fd77267e3f280d1cd0869c9de117ec6b4c496c0c
AdemJensen <rentenglong@163.com>
2017-06-29 17:32:46 +0800

    style: change language jack buttons' link
    Some of the pictures of the original UOJ system uses the picture bed that belongs to the real UOJ which set up by vfleaking. But it may not work on some situations of the community version.
    This commit added language flag pictures and changed the link to those pictures.

--- uoj/1/app/views/page-footer.php
+++ uoj/1/app/views/page-footer.php
@@ -7,14 +7,14 @@
 			<?php if ($ShowPageFooter): ?>
 			<div class="uoj-footer">
 				<p>
-					<a href="<?= HTML::url(UOJContext::requestURI(), array('params' => array('locale' => 'zh-cn'))) ?>"><img src="//img.uoj.ac/utility/flags/24/cn.png" alt="中文" /></a> 
-					<a href="<?= HTML::url(UOJContext::requestURI(), array('params' => array('locale' => 'en'))) ?>"><img src="//img.uoj.ac/utility/flags/24/gb.png" alt="English" /></a>
+					<a href="<?= HTML::url(UOJContext::requestURI(), array('params' => array('locale' => 'zh-cn'))) ?>"><img src="/pictures/lang/cn.png" alt="中文" /></a> 
+					<a href="<?= HTML::url(UOJContext::requestURI(), array('params' => array('locale' => 'en'))) ?>"><img src="/pictures/lang/gb.png" alt="English" /></a>
 				</p>
 				
 				<ul class="list-inline">
 					<li><?= UOJConfig::$data['profile']['oj-name'] ?></li>
-					<?php if (UOJConfig::$data['profile']['ICP-license']!=''): ?>
-					 | <li><a href="http://www.miitbeian.gov.cn"><?= UOJConfig::$data['profile']['ICP-license'] ?></a></li>
+					<?php if (UOJConfig::$data['profile']['ICP-license'] != ''): ?> | 
+					<li><a href="http://www.miitbeian.gov.cn"><?= UOJConfig::$data['profile']['ICP-license'] ?></a></li>
 					<?php endif ?>
 				</ul>
 				
