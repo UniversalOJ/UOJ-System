94c827e5b3205a213662950eb3774356bb69671d
ceabrobot <ceba_robot@163.com>
2017-11-25 16:53:54 +0000

    feat(install/bundle): change image and packages to ubuntu16.04
    Change the base Docker image to ubuntu16.04 and change the LAMP
    packages to support PHP7.
    
    BREAKING CHANGE: the packages which will be installed has changed.

--- install/bundle/install.sh
+++ install/bundle/install.sh
@@ -13,10 +13,10 @@ _svn_certroot_password_=$(genRandStr 32)
 getAptPackage(){
 	echo -e "\n\n==> Getting environment packages"
 	#Set MySQL root password
-	debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password $_database_password_" && debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password $_database_password_"
+	debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password $_database_password_" && debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password $_database_password_"
 	#Update apt sources and install
-	apt-get update -y
-	apt-get install -y vim ntp zip unzip curl wget subversion apache2 libapache2-mod-xsendfile libapache2-mod-auth-mysql php5 php5-dev php-pear php5-mysql mysql-server-5.6 cmake fp-compiler re2c libv8-dev libyaml-dev python python3 python-requests
+    apt-get update -y
+    apt install -y vim ntp zip unzip curl wget subversion apache2 mysql-server mysql-client php libapache2-mod-php7.0 php7.0-mysql cmake fp-compiler re2c libv8-dev libyaml-dev python python3 python-requests
 	#Install PHP extensions
 	echo -e "\n\n" | pecl install v8js-0.1.3 yaml-1.3.1
 }
