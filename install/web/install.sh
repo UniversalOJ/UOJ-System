d0cea32f88d176cb794f053ca370853c59823873
Baoshuo <i@baoshuo.ren>
2022-03-17 16:26:04 +0800

    chore(web): upgrade to ubuntu 20.04

--- install/web/install.sh
+++ install/web/install.sh
@@ -14,9 +14,11 @@ getAptPackage(){
     export DEBIAN_FRONTEND=noninteractive
     dpkg -s gnupg 2>/dev/null || (apt-get update && apt-get install -y gnupg)
     echo "deb http://ppa.launchpad.net/stesie/libv8/ubuntu bionic main" | tee /etc/apt/sources.list.d/stesie-libv8.list && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D858A0DF
-    apt-get update && apt-get install -y vim ntp zip unzip curl wget apache2 libapache2-mod-xsendfile libapache2-mod-php php php-dev php-pear php-zip php-mysql php-mbstring g++ cmake re2c libv8-7.5-dev libyaml-dev
+    apt-get update && apt-get install -y git vim ntp zip unzip curl wget apache2 libapache2-mod-xsendfile libapache2-mod-php php php-dev php-pear php-zip php-mysql php-mbstring g++ cmake re2c libv8-7.5-dev libyaml-dev
     #Install PHP extensions
-    printf "/opt/libv8-7.5\n\n" | pecl install v8js yaml
+    yes | pecl install yaml
+    git clone https://github.com/phpv8/v8js.git --depth=1 /tmp/pear/download/v8js-master && cd /tmp/pear/download/v8js-master
+    phpize && ./configure --with-php-config=/usr/bin/php-config --with-v8js=/opt/libv8-7.5 && make install && cd -
 }
 
 setLAMPConf(){
@@ -45,7 +47,7 @@ UOJEOF
     a2enmod rewrite headers && sed -i -e '172s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
     #Create UOJ session save dir and make PHP extensions available
     mkdir --mode=733 /var/lib/php/uoj_sessions && chmod +t /var/lib/php/uoj_sessions
-    sed -i -e '865a\extension=v8js.so\nextension=yaml.so' /etc/php/7.2/apache2/php.ini
+    sed -i -e '912a\extension=v8js.so\nextension=yaml.so' /etc/php/7.2/apache2/php.ini
 }
 
 setWebConf(){
@@ -66,7 +68,7 @@ UOJEOF
     cat >include/uoj_work_path.h <<UOJEOF
 #define UOJ_WORK_PATH "/opt/uoj/judger/uoj_judger"
 #define UOJ_JUDGER_BASESYSTEM_UBUNTU1804
-#define UOJ_JUDGER_PYTHON3_VERSION "3.6"
+#define UOJ_JUDGER_PYTHON3_VERSION "3.8"
 #define UOJ_JUDGER_FPC_VERSION "3.0.4"
 UOJEOF
     make runner -j$(($(nproc) + 1)) && cd ../../install/web
